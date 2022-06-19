89ad9060d11f650bf872a95525cc091df5a1f0b0
Ghislain <ghislain.de_clarens@ensam.eu>
2022-06-19 04:17:23 +0200

    resolution bug urdf

--- cdpr/urdf/gen_cable_2.py
+++ cdpr/urdf/gen_cable_2.py
@@ -115,24 +115,28 @@ if __name__ == '__main__':
 
             collision = etree.SubElement(link, 'collision')
             l_xyz = ('%f %f %f %f %f %f' % tuple(cp + rpy_gen)).split(" ")
-            print("position cable "+str(l_xyz))
-            xyz_c = str(l_xyz[0]+" "+l_xyz[1]+" "+l_xyz[2])
+            l_xyz_2=('%f %f %f %f %f %f' % tuple(cbl.frame + rpy_gen)).split(" ")
+            #xyz_c = str(l_xyz[0]+" "+l_xyz[1]+" "+l_xyz[2])
+            xyz_c =str(l_xyz[0]+" "+l_xyz[1]+" "+l_xyz[2])
+            offset = np.linalg.norm(np.array(cbl.frame)-pp.reshape(3))-l/2
+            print(offset)
             rpy= str(l_xyz[3]+" "+l_xyz[4]+" "+l_xyz[5])
 
-            origin=etree.SubElement(collision, 'origin', xyz = xyz_c, rpy = rpy)
+            origin=etree.SubElement(collision, 'origin', xyz ='0 0 '+str(offset), rpy = "0 0 0")
             geometry=etree.SubElement(collision, 'geometry')
             cylinder=etree.SubElement(geometry, 'cylinder',radius = str(config.cable.radius), length = str(l))
 
             visual = etree.SubElement(link, 'visual')
 
-            origin=etree.SubElement(visual, 'origin', xyz = xyz_c, rpy = rpy)
+            #origin=etree.SubElement(visual, 'origin', xyz = xyz_c, rpy = rpy)
+            origin=etree.SubElement(visual, 'origin', xyz ='0 0 '+str(offset), rpy = "0 0 0")
             geometry=etree.SubElement(visual, 'geometry')
             cylinder=etree.SubElement(geometry, 'cylinder',radius = str(config.cable.radius), length = str(l))
             material=etree.SubElement(visual, 'material', name = "${cable_color}")
 
             inertial = etree.SubElement(link, 'inertial')
 
-            origin=etree.SubElement(inertial, 'origin', xyz = xyz_c, rpy = rpy)
+            origin=etree.SubElement(inertial, 'origin', xyz ='0 0 '+str(offset), rpy = "0 0 0")
             geometry=etree.SubElement(inertial, 'geometry')
             cylinder=etree.SubElement(geometry, 'cylinder',radius = str(config.cable.radius), length = str(l))
             inertia=etree.SubElement(inertial, 'inertia', ixx="0.001", ixy="0", ixz="0", iyy="0.001", iyz="0", izz="0.001")
@@ -149,10 +153,11 @@ if __name__ == '__main__':
             #CreateVisualCollision(link,'/geometry/cylinder/radius', .03, color='Red', collision=False)
             #CreateNested(link, 'visual/geometry/cylinder/length', 0.3)
             visual = etree.SubElement(link, 'visual')
-            origin=etree.SubElement(inertial, 'origin', xyz = xyz, rpy = rpy)
+            origin=etree.SubElement(inertial, 'origin', xyz = "0 0 0", rpy = "0 0 0")
             geometry=etree.SubElement(visual, 'geometry')
             cylinder=etree.SubElement(geometry, 'box',size = "0.1 0.1 0.1")
             material=etree.SubElement(visual, 'material', name = "${cable_color}")
+            origin=etree.SubElement(visual, 'origin', xyz = "0 0 0", rpy = "0 0 0")
 
             # revolute joint around X
             joint = etree.SubElement(robot, 'joint', name= 'rev_X%i' % i)
@@ -175,21 +180,23 @@ if __name__ == '__main__':
             xyz_vlx = str(l_4[0]+" "+l_4[1]+" "+l_4[2])
             rot_vlx = str(l_4[3]+" "+l_4[4]+" "+l_4[5])
 
-            origin=etree.SubElement(inertial, 'origin', xyz = xyz_vlx, rpy = rot_vlx)
+            origin=etree.SubElement(inertial, 'origin', xyz = "0 0 0", rpy = "0 0 0")
             mass=etree.SubElement(inertial, 'mass', value = "0.001")
             inertia=etree.SubElement(inertial, 'inertia', ixx="0.001", ixy="0", ixz="0", iyy="0.001", iyz="0", izz="0.001")
             visual = etree.SubElement(link, 'visual')
-            origin=etree.SubElement(inertial, 'origin', xyz = xyz_vlx, rpy = rot_vlx)
+            origin=etree.SubElement(visual, 'origin', xyz = "0 0 0", rpy = "0 0 0")
             geometry=etree.SubElement(visual, 'geometry')
             cylinder=etree.SubElement(geometry, 'box',size = "0.1 0.1 0.1")
             material=etree.SubElement(visual, 'material', name = "${cable_color}")
+
+
             # revolute joint around Y
             joint = etree.SubElement(robot, 'joint', name= 'rev_Y%i' % i)
             joint.set("type", "revolute")   
             l_5 = ('0 0 0 %f %f %f' % tuple(rpy_gen)).split(" ")
             xyz_rjx = str(l_5[0]+" "+l_5[1]+" "+l_5[2])
             rot_rjx = str(l_5[3]+" "+l_5[4]+" "+l_5[5])
-            origin=etree.SubElement(joint, 'origin', xyz = "0 0 0", rpy = rot_rjx)
+            origin=etree.SubElement(joint, 'origin', xyz = "0 0 0", rpy = "0 0 0")
             parent_link=etree.SubElement(joint, 'parent', link = 'virt_X%i' % i)
             child_link=etree.SubElement(joint, 'child', link = 'virt_Y%i' % i)
             axe=etree.SubElement(joint, 'axis', xyz = '%f %f %f' % tuple(R[:3,1]))
@@ -201,21 +208,20 @@ if __name__ == '__main__':
             # prismatic joint
             joint = etree.SubElement(robot, 'joint', name= 'joint_cable%i' % i)
             joint.set("type", "prismatic")
-            l_6 = ('0 0 %f %f %f %f' % tuple([(a-1.)*l/2] + rpy_gen)).split(" ")
-            #l_6 = ('0 0 0 %f %f %f' % tuple(rpy_gen)).split(" ")
-            print(l_6)
+            #l_6 = ('0 0 %f %f %f %f' % tuple([(a-1.)*l/2] + rpy_gen)).split(" ")
+            l_6 = ('0 0 0 %f %f %f' % tuple(rpy_gen)).split(" ")
             xyz_pjx = str(l_6[0]+" "+l_6[1]+" "+l_6[2])
             rot_pjx = str(l_6[3]+" "+l_6[4]+" "+l_6[5])
             print("prismatic" + str(str(float(l_2[0])-float(l_xyz[0]))+" "+str(float(l_2[1])-float(l_xyz[1]))+" "+str(float(l_2[2])-float(l_xyz[2]))))
-            origin=etree.SubElement(joint, 'origin', xyz = str(str(float(l_2[0])-float(l_xyz[0]))+" "+str(float(l_2[1])-float(l_xyz[1]))+" "+str(float(l_2[2])-float(l_xyz[2]))), rpy = rot_pjx)
+            #origin=etree.SubElement(joint, 'origin', xyz = str(str(float(l_2[0])-float(l_xyz[0]))+" "+str(float(l_2[1])-float(l_xyz[1]))+" "+str(float(l_2[2])-float(l_xyz[2]))), rpy = rot_pjx)
+            origin=etree.SubElement(joint, 'origin', xyz = xyz_pjx, rpy= "0 0 0")
             parent_link=etree.SubElement(joint, 'parent', link = 'virt_Y%i' % i)
             child_link=etree.SubElement(joint, 'child', link = 'cable%i' % i)
             axe=etree.SubElement(joint, 'axis', xyz = '%f %f %f' % tuple(-R[:3,2]))
             limit=etree.SubElement(joint, 'limit',lower = str(-0.5*l), upper = str(0.5*l), effort=str(config.joints.passive.effort), velocity=str(config.joints.passive.velocity))
             dynamics=etree.SubElement(joint, 'limit', damping=str(config.joints.passive.damping)) 
-
-
-            # rotation cable/pf X  
+            origin=etree.SubElement(joint, 'origin', xyz = "0 0 0", rpy=rot_pjx)
+            # rotation cable/pf X
 
             link = etree.SubElement(robot, 'link', name= 'virt_Xpf%i' % i)
             inertial = etree.SubElement(link, 'inertial')
@@ -238,14 +244,14 @@ if __name__ == '__main__':
             l_8 = ('0 0 0 %f %f %f' % tuple(rpy_gen)).split(" ")
             xyz_rjx = str(l_8[0]+" "+l_8[1]+" "+l_8[2])
             rot_rjx = str(l_8[3]+" "+l_8[4]+" "+l_8[5])
-            origin_xpf=etree.SubElement(joint, 'origin', xyz = xyz_rjx, rpy = rot_rjx)
+            origin_xpf=etree.SubElement(joint, 'origin', xyz = "0 0 0", rpy ="0 0 0")
             parent_link=etree.SubElement(joint, 'parent', link = 'platform')
             child_link=etree.SubElement(joint, 'child', link = 'virt_Xpf%i' % i)
             axe=etree.SubElement(joint, 'axis', xyz = '1 0 0')
             limit=etree.SubElement(joint, 'limit', effort=str(config.joints.passive.effort), velocity=str(config.joints.passive.velocity))
             dynamics=etree.SubElement(joint, 'limit', damping=str(config.joints.passive.damping))
 
-
+            """
             # rotation cable/pf Y
             link = etree.SubElement(robot, 'link', name= 'virt_Ypf%i' % i)
             l_10 = ('%f %f %f %f %f %f' % tuple(list(pp.reshape(3)) + rpy_gen)).split(" ")
@@ -287,7 +293,7 @@ if __name__ == '__main__':
             axe=etree.SubElement(joint, 'axis', xyz = '0 0 1')
             limit=etree.SubElement(joint, 'limit', effort=str(config.joints.passive.effort), velocity=str(config.joints.passive.velocity))
             dynamics=etree.SubElement(joint, 'limit', damping=str(config.joints.passive.damping)) 
-
+            """
             # write file
             WriteXACRO(robot, name+'.xacro')
 
